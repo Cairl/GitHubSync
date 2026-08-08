23b7a2e9d905b6ac8cedfaa8f7d2e404d8782963
Cairl <Cairl@users.noreply.github.com>
2026-08-08 10:16:20 +0800

    Update: 2026-08-08 10:16:20

--- github_sync.bat
+++ github_sync.bat
@@ -1,5 +1,5 @@
 @echo off
-set "PROJECT_DIR=S:\Github Repositories\GitHubSync"
+set "PROJECT_DIR=S:\GitHubSync"
 set "SYNC_TARGET=%~dp0"
 set "PYTHONPATH=%PROJECT_DIR%"
 python -m src "%SYNC_TARGET:~0,-1%"
